a770270c77c4d569e66810b8bc22909cd91aa635
hex <idontknowbrotherpls@gmail.com>
2022-03-21 12:01:02 +0700

    added: get building by id

--- routes/buildings.js
+++ routes/buildings.js
@@ -8,17 +8,28 @@ const getBuildings = async function(req, res, next){
 }
 
 const addBuilding = async function(req, res, next){
-    const building = new Building({
-        name: req.body.name,
-        level: req.body.level
-    })
-    await building.save()
-    res.status(201).json(building)
+    try {
+        const building = new Building({
+            name: req.body.name,
+            level: req.body.level
+        })
+        await building.save()
+        res.status(201).json(building)
+    } catch (err) {
+        res.status(409).json({message: err.message})
+    }
+}
+
+const getBuilding = async function(req, res, next){
+    console.log(req.params.id);
+    const building = await Building.findById(req.params.id)
+    res.status(200).json(building)
 }
 
 /* GET home page. */
 router.get('/', getBuildings)
 router.post('/', addBuilding)
+router.get('/:id', getBuilding)
 
 module.exports = router
 
